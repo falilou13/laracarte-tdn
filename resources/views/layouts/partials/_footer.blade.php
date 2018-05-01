20154489fc39387ebf19e251eb2179faa195310c
Falilou Diallo <faliloudiallo13@gmail.com>
2018-05-01 03:34:12 +0000

    Setup and add static pages

---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="text-content">
+	<p>			&copy; {{ date('Y') }} &middote; Laracarte by 
+		<a href="https://twitter.com/falilou13">@falilou</a> &dash; A 
+		<a href="https://laramap.com">Laramap clone app.</a>
+	</p>
+
+	<p><b>This app has been built for learning purposes.</b></p>
+</footer>
